0289ac5c8ffe73a5f56510e30d3a48bbac5f9b44
Peter Darinzo <birel44@yahoo.com>
2022-02-07 15:09:41 -0800

    use payload for emojis

--- moodChanger.js
+++ moodChanger.js
@@ -7,35 +7,36 @@ const confusedButton = document.querySelector("#confused");
 const devilishButton = document.querySelector("#devilish");
 const randomButton = document.querySelector("#random");
 
-const MOODS = ["HAPPY", "SLEEPING", "CONFUSED", "DEVILISH"];
+const MOODS = { "HAPPY": "＼(＾▽＾)／", "SLEEPING": "(ᴗ˳ᴗ)", "CONFUSED": "ఠ_ఠ", "DEVILISH": "( ｀▽´ )Ψ" };
 
 happyButton.addEventListener("click", () => {
-  store.dispatch({ type: "HAPPY" });
+  store.dispatch({ type: "HAPPY", payload: MOODS.HAPPY });
   const state = store.getState();
   h1.innerText = state.emoji;
 });
 
 sleepyButton.addEventListener("click", () => {
-  store.dispatch({ type: "SLEEPING" });
+  store.dispatch({ type: "SLEEPING", payload: MOODS.SLEEPING });
   const state = store.getState();
   h1.innerText = state.emoji;
 });
 
 confusedButton.addEventListener("click", () => {
-  store.dispatch({ type: "CONFUSED" });
+  store.dispatch({ type: "CONFUSED", payload: MOODS.CONFUSED });
   const state = store.getState();
   h1.innerText = state.emoji;
 });
 
 devilishButton.addEventListener("click", () => {
-  store.dispatch({ type: "DEVILISH" });
+  store.dispatch({ type: "DEVILISH", payload: MOODS.DEVILISH });
   const state = store.getState();
   h1.innerText = state.emoji;
 })
 
 randomButton.addEventListener("click", () => {
   const randomChoice = Math.floor(Math.random() * 4);
-  store.dispatch({ type: MOODS[randomChoice] });
+  const mood = (Object.keys(MOODS))[randomChoice];
+  store.dispatch({ type: mood, payload: MOODS[mood] });
   const state = store.getState();
   h1.innerText = state.emoji;
 })
--- reduxSetup.js
+++ reduxSetup.js
@@ -4,13 +4,13 @@ const INTIIAL_STATE = { emoji: "(^人^)" };
 const emojiReducer = (state = INTIIAL_STATE, action) => {
   switch (action.type) {
     case "HAPPY":
-      return { ...state, emoji: "＼(＾▽＾)／" };
+      return { ...state, emoji: action.payload };
     case "CONFUSED":
-      return { ...state, emoji: "ఠ_ఠ" };
+      return { ...state, emoji: action.payload };
     case "SLEEPING":
-      return { ...state, emoji: "(ᴗ˳ᴗ)" };
+      return { ...state, emoji: action.payload };
     case "DEVILISH":
-      return { ...state, emoji: "( ｀▽´ )Ψ" };
+      return { ...state, emoji: action.payload };
     default:
       return state;
   }
